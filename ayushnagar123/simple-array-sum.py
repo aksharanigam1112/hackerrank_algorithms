2791ad789236f6dce67adf12fc0ebd454c515726
Ayush Nagar <32647423+ayushnagar123@users.noreply.github.com>
2019-01-14 23:56:53 +0530

    simple array sum question solution

---
+++ ayushnagar123/simple-array-sum.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the simpleArraySum function below.
+#
+def simpleArraySum(ar,ar_count):
+    #
+    # Write your code here.
+    #
+    sum1=0
+    for i in range(ar_count):
+        sum1=sum1+ar[i]
+    return sum1
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar,ar_count)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
