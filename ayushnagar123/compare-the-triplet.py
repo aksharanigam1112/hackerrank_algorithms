3d44c69d9851e9c5e3578a7a8564b0b73a86ab22
Ayush Nagar <32647423+ayushnagar123@users.noreply.github.com>
2019-01-15 00:08:02 +0530

    compare the triplet question solution

---
+++ ayushnagar123/compare-the-triplet.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    ac=bc=0
+    for i in range(3):
+        if(a[i]>b[i]):
+            ac=ac+1
+        elif(b[i]>a[i]):
+            bc=bc+1
+    return ac,bc
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
