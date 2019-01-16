022b4fabfcd8768eef19a387962e694ff07a6e7f
Ayush Nagar <32647423+ayushnagar123@users.noreply.github.com>
2019-01-16 23:37:26 +0530

    drawing book question soluton

---
+++ ayushnagar123/Drawing-book.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the pageCount function below.
+ */
+int pageCount(int n, int p) {
+    /*
+     * Write your code here.
+     */
+    int k=0;
+    int pg=n/2;
+    int r=p/2;
+    int l=pg-r;
+    if(r<l)
+        return r;
+    else
+        return l;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int p;
+    cin >> p;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = pageCount(n, p);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
